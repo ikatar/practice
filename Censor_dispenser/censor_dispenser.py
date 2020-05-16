0b2e4107c25e6b287efd3f64f2cc59d9a166d6a6
ikatar <underwoodvfx@gmail.com>
2020-05-16 20:39:21 +0100

    Censor Dispenser Challenge Project(Python)
    Fixed algorithm to match requirements

--- Censor_dispenser/censor_dispenser.py
+++ Censor_dispenser/censor_dispenser.py
@@ -2,38 +2,47 @@ import re
 
 def censor(text,words_to_censor,double_occurrences):
 	#removes words and negative words after 2 occurrences
+	original_text = text
 	text = text.lower()
-	for word in words_to_censor:
-		#text = text.lower().replace(word," === ")
-		text = re.sub(r"\b{w}\b".format(w=word),"X"*len(word),text)
+	to_remove=[]
 
-	count = {}
-	count_sorted = {}
+	for word in words_to_censor:
+		word = re.compile(r"\b{word}\b".format(word=word))
+		for m in word.finditer(text):
+			to_remove.append([[m.group()],[m.start(),m.end()]])
+	for word in to_remove:
+		original_text = original_text[:word[1][0]] + "X"*len(word[0][0]) + original_text[word[1][1]:]
+
+	to_remove =[]
+
 	for word in double_occurrences:
-		if word in text:
-			try:
-				count[word]= re.search(r"\b{word}\b".format(word=word),text).start()
-			except Exception:
-				pass
-	for k in sorted(count,key=count.get):
-		count_sorted[k] = count[k]
+		word = re.compile(r"\b{word}\b".format(word=word))
+		for m in word.finditer(text):
+			to_remove.append([[m.group()],[m.start(),m.end()]])
 	
-	to_remvoe=list(count_sorted.keys())
+	to_remove.sort(key=lambda x: x[1][1])
 	
-	for i in to_remvoe[2:]:
-		text = text.replace(i,"X"*len(i))
-	return text
+	for word in to_remove[2:]:
+		original_text = original_text[:word[1][0]] + "X"*len(word[0][0]) + original_text[word[1][1]:]
+
+	return original_text
 
 def censor_plus_next_words(text,list1,list2):
 	big_list = list1+list2
+	original_text = text
 	text = text.lower()
+
+	to_remove =[]
+
 	for word in big_list:
-		search_pattern = re.compile(r"\w*['-_]?\w? {w} \w*['-_]?\w?".format(w=word))
-		length = search_pattern.findall(text)
-		if length:
-			#print(length[0],len(length[0]))
-			text = re.sub(r"\w*['-_]?\w? {w} \w*['-_]?\w?".format(w=word),"X"*len(length[0])+"",text)
-	return text
+		word = re.compile(r"\b\w*['-]?\w*\b ?\b{word}\b ?\b\w*['-]?\w*\b".format(word=word))
+		for m in word.finditer(text):
+			to_remove.append([[m.group()],[m.start(),m.end()]])
+	to_remove.sort(key=lambda x: x[1][1])
+
+	for word in to_remove:
+		original_text = original_text[:word[1][0]] +"X"*len(word[0][0])+ original_text[word[1][1]:]
+	return original_text
 
 email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
@@ -41,7 +50,8 @@ email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
 
 proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithms ", "her", "herself"]
-negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
+negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressing", "distressed", "concerning", "horrible", "horribly", "questionable"]
 
-print(censor(email_three,proprietary_terms,negative_words ))
+print(censor(email_three,proprietary_terms,negative_words))
 print(censor_plus_next_words(email_four,proprietary_terms,negative_words))
+
